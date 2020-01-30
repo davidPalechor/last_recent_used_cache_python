fee53a353351cd8d42e5b4340500d55cc169e212
Sergio Palechor <davidpalechor95@hotmail.com>
2020-01-30 17:08:26 -0500

    Added table driven tests on input validation

--- axis_overlap/tests/test_overlap.py
+++ axis_overlap/tests/test_overlap.py
@@ -45,8 +45,44 @@ class TestOverlap(unittest.TestCase):
         ]
 
         for test in tests:
-            print(test['name'])
             self.assertEqual(test['response'], overlaps(*test['data']))
+
+    def test_validate_input(self):
+        tests = [
+            {
+                'name': 'Valid input',
+                'data': ('1,6', '6,5'),
+                'response': True
+            },
+            {
+                'name': 'Valid input (floating point numbers)',
+                'data': ('1.6576,6.78989', '6.77377,5.8909888'),
+                'response': True
+            },
+            {
+                'name': 'Valid input (negative floating point numbers)',
+                'data': ('-1.6576,6.78989', '-6.77377,5.8909888'),
+                'response': True
+            },
+            {
+                'name': 'Invalid input',
+                'data': ('1.9.3,9', '1.3.4,9.3.1'),
+                'response': False
+            },
+            {
+                'name': 'Invalid input',
+                'data': ('1.9,9', '1.3.4,9.3.1'),
+                'response': False
+            },
+            {
+                'name': 'Invalid input',
+                'data': ('1ab,9', '1.3,9bv'),
+                'response': False
+            }
+        ]
+
+        for test in tests:
+            self.assertEqual(test['response'], bool(validate_input(*test['data'])))
 
 
 if __name__ == '__main__':
